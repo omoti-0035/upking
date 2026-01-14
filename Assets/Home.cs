87eac144a3d51e88224b56d4f4852064e3cff1ec
moyasi <250372@jc-21.jp>
2026-01-15 00:22:45 +0900

    音声の追加 リザルト画面の変更

---
+++ Assets/Home.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Home : MonoBehaviour
+{
+        public AudioSource seSource;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+ public void HomeButton()
+    {
+        seSource.Play();
+        SceneManager.LoadScene("Stert");
+    }
+}
