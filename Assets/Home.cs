3f54f2b148a9f3c74def2376da4af1b753ef5dae
omoti-0035 <glafa0035@gmail.com>
2026-02-19 17:30:05 +0900

    ちょっと変更

--- Assets/Home.cs
+++ Assets/Home.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class Home : MonoBehaviour
 {
-        public AudioSource seSource;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
- public void HomeButton()
+    public AudioSource seSource;
+    public void HomeButton()
     {
-        seSource.Play();
-        SceneManager.LoadScene("Stert");
-        Destroy(GameObject.Find("GameDirector"));
+        {
+            seSource.Play();
+            SceneManager.LoadScene("Stert");
+        }
     }
 }
